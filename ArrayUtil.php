47d2c4cf30b3843843f7185827dc27fd072a4e93
biggins <sam@evoluted.net>
2014-03-26 16:53:38 +0000

    Added Cartesian Array combine function

--- ArrayUtil.php
+++ ArrayUtil.php
@@ -104,5 +104,50 @@ class ArrayUtil
 
 		return null;
 	}
+	
+	/**
+	 * Produces the cartesian product of all the given arrays
+	 * 
+	 * @param array Array of Arrays to combine e.g. array( array('a', 'b'), array('1', '2') )
+	 * @return array Array of products e.g. array( array('a', '1'), array('a', '2'), array('b', '1'), array('b', '2')) 
+	 */
+	public static function array_cartesian_product($arrays)
+	{
+	    $result = array();
+	    $arrays = array_values($arrays);
+	    $sizeIn = sizeof($arrays);
+	    $size = $sizeIn > 0 ? 1 : 0;
+	    foreach ($arrays as $array) {
+	    	
+	    	$size = $size * sizeof($array);
+	    	
+	    }
+	        
+	    for ($i = 0; $i < $size; $i ++) {
+	    	
+	        $result[$i] = array();
+	        for ($j = 0; $j < $sizeIn; $j ++) {
+	        	
+	            array_push($result[$i], current($arrays[$j]));	        	
+	            
+	        }
+
+	        for ($j = ($sizeIn -1); $j >= 0; $j --) {
+	        	
+	            if (next($arrays[$j])) {
+	            	
+	            	break;
+	            	
+	            } elseif (isset ($arrays[$j])) {
+	            	
+	            	reset($arrays[$j]);
+	            	
+	            }
+	                
+	        }
+	        
+	    }
+	    return $result;
+	}
 
 }
